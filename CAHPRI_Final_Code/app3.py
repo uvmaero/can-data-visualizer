383795f3405610a1feb41bc9a2ce87314fc523ce
georgespearing <spearingg@gmail.com>
2020-12-04 10:55:30 -0500

    change javascript

--- CAHPRI_Final_Code/app3.py
+++ CAHPRI_Final_Code/app3.py
@@ -12,7 +12,7 @@ import time
 import can
 from datetime import datetime
 
-from flask import Flask, render_template, request, Response, json
+from flask import Flask, render_template, request, Response, json, jsonify
 
 app = Flask(__name__)
 
@@ -24,21 +24,32 @@ data = 0
 
 # Generate json_dump file for data. This uses external script "can_logger"
 def get_message_value(can_id, bit):
-    print("can")
-    while True:
-        print("help me")
-        for message in bus:
-            if message.arbitration_id == can_id:
-                data = message.data[bit]
-            else:
-                data=0
-        
-        json_data = json.dumps(
-                {'time':datetime.now().strftime('%H:%M:%S'), 'value': data})
-        yield f"data:{json_data}\n\n"
-        # delay system to ensure stability3
-        print(data)
-        time.sleep(.1)
+    # print("get message")
+    # print("can")
+    # while True:
+    # print("help me")
+    # for message in bus:
+    #     if message.arbitration_id == can_id:
+    #         data = message.data[bit]
+    #         print(f"yay {data}")
+    #     else:
+    #         data=0
+
+    msg = can.Message(is_extended_id=False,arbitration_id=can_id)
+    print(msg)
+    print(bytearray.decode(msg.data))
+    
+    # json_data = json.dumps(
+    #         {"time":datetime.now().strftime('%H:%M:%S'), "value":data})
+    json_data = f'"time":0,"value":{data}'
+    # return f'data:{json_data}\n\n'
+    print(json_data)
+    return json_data    
+    # yield json_data
+
+    # delay system to ensure stability3
+    print(json_data)
+    time.sleep(.1)
 
 
 # Landing Page
@@ -57,8 +68,18 @@ def throttleData():
 
 @app.route('/faults')
 def faultData():
-    print("faults")
-    return Response(get_message_value(0x079, 0), mimetype='text/event-stream')
+    # print("faults")
+    event = 'data:{"value":1}\n\n'
+    data_return = 'data:{'+str(get_message_value(0x079, 0))+'}\n\n'
+    # return Response(get_message_value(0x079, 0), mimetype='text/event-stream')
+    # return Response('data:{"value":1}\n\n', mimetype='text/event-stream')
+    return Response(data_return, mimetype='text/event-stream')
+
 
 if __name__ == '__main__':
-	application.run(debug==True, threaded=True)
+    while True:
+        data_return = 'data:{'+str(get_message_value(0x079, 0))+'}\n\n'
+        print(data_return)
+        time.sleep(0.01)
+    
+	# app.run(threaded=True)
